5c5ec576379f4692e54c106e39d42cc18862ca8c
edgeandrew <andrew@edgeonline.com.au>
2023-03-14 08:08:22 +1000

    Update set.lua

--- nvim/lua/syphonb/set.lua
+++ nvim/lua/syphonb/set.lua
@@ -11,7 +11,7 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = "~/.backup/"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
